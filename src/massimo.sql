83980354b39906cb6733e42778a0573d63cf07b2
venuti <massimo.venuti@etu.unistra.fr>
2020-12-11 16:17:59 +0100

    fin trigger 1

--- src/massimo.sql
+++ src/massimo.sql
@@ -1,9 +1,13 @@
 set serveroutput on;
 
-/*
+/**
  * Procédures et fonctions PL/SQL
  */
 
+/**
+ * 2. Définir une procédure qui généra un texte initial de la newsletter en y ajoutant la liste de
+ * toute les sortie de la semaine.
+ */
 create or replace procedure newsletter
 is
 begin 
@@ -21,6 +25,9 @@ end;
 /
 
 /**
+ * 3. Générer la liste des vidéos populaires, conseillé pour un utilisateur, c’est à dire fonction des
+ * catégories de vidéos qu’il suit.
+ * 
  * Des suggestions de vidéos à regarder seront aussi générées. Elles seront basées uniquement sur la
  * popularité des vidéos par catégories. La popularité sera basée sur le nombre de visionnages au cours
  * des deux dernières semaines afin de favoriser les vidéos récentes.
@@ -62,9 +69,44 @@ begin
 end;
 /
 
-/*
+/**
  * Contraintes d'intégrité
  */
 
-create trigger favs
-after insert on favorite
+/**
+ * 1. Un utilisateur aura un maximum de 300 vidéos en favoris
+ */
+create or replace trigger favs
+before insert on favorite
+for each row
+declare
+    nbfavs_v integer;
+begin
+    select count(*) into nbfavs_v
+    from favorite
+    where idclient = :new.idclient;
+    
+    if nbfavs_v >= 300 then
+        raise_application_error(-20000, 'Nombre de favoris maximal atteint pour l''utilisateur ' ||  :new.idclient);
+    end if;
+end;
+/
+
+create or replace procedure test_trigger_favs 
+is
+begin
+    FOR i IN 100..399 LOOP
+          insert into video values (i, 1, '../videos/path', to_char(i), 'desc', 32, 2000, SYSDATE-2,  SYSDATE+10, SYSDATE+12, 1, 'FR', '720p', 'AVI');
+          insert into favorite values (1, i);
+          commit;
+        END LOOP;
+    insert into video values (400, 1, '../videos/path', '400', 'desc', 32, 2000, SYSDATE-2, SYSDATE+10, SYSDATE+12, 1, 'FR', '720p', 'AVI');
+    insert into favorite values (1, 400);    
+    commit;
+end;
+/
+
+begin
+    test_trigger_favs;
+end;
+/
